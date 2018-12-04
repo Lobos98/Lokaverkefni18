1a86fa06c7cd4d6d5656b8b1e92ba8b34351c7fe
liljag18 <liljag18@ru.is>
2018-12-04 13:10:41 +0000

    get functions created

--- models/Customer.py
+++ models/Customer.py
@@ -8,3 +8,24 @@ class Customer:
 		self.__banned = False
 		self.__history = {}
 		self.__orders = []
+
+	def get_name(self):
+		return self.__name
+
+	def get_email(self):
+		return self.__email
+
+	def get_phone_no(self):
+		return self.__phone_no
+
+	def get_ssn(self):
+		return self.__ssn
+
+	def get_orders(self):
+		return self.__orders
+
+	def get_history(self):
+		return self.__history
+
+	def get_banned(self):
+		return self.__banned

e0df9f1d83e9ed0d8b3d1d64d1b55c6951ffe179
Lobos <robertelis98@gmail.com>
2018-12-04 15:07:35 +0000

    Útfærði Car klasann og öll föll og eigindi fyrir hann

--- models/Car.py
+++ models/Car.py
@@ -1,3 +1,4 @@
+from datetime import datetime, timedelta
 
 
 class Car:
@@ -13,11 +14,69 @@ class Car:
         else:
             self.__history = history
         if reserved_dates == None:
-            self.__reserved_dates = dict()
+            self.__reserved_dates = set()
         else:
             self.__reserved_dates = reserved_dates
 
+    def __repr__(self):
+        return "Car({}, {}, {}, {}, {}, {}, {})".format(self.__reg_num,\
+        self.__type, self.__color, self.__model, self.__broken,\
+        self.__history.__repr__(), self.__reserved_dates.__repr__())
 
-a = Car("BX-463", "Jeppi", "Rauður", "1998")
+    def __str__(self):
+        return "{} er {} {} frá {}".format(self.__reg_num, self.__color,\
+        self.__type, self.__model)
 
-print(a)
+    def get_reg_num(self):
+        return self.__reg_num
+
+    def get_model(self):
+        return self.__model
+
+    def get_type(self):
+        return self.__type
+
+    def get_color(self):
+        return self.__color
+    
+    def get_broken(self):
+        return self.__broken
+
+    def get_history(self):
+        ###Skilar uppflettitöflu þar sem hver lykill er tilgreindur með
+        ###tölvupóstfangi og gildi lykilsins er listi af túplum, þar sem
+        ###hver túpla inniheldur leigudag og skiladag.
+        return self.__history
+
+    def get_reserved_dates(self):
+        return self.__reserved_dates
+
+    def change_broken_status(self):
+        if self.__broken == True:
+            self.__broken = False
+        else:
+            self.__broken = True
+    
+    def add_reservation(self, order):
+        pickup_date = order.get_pickup_date()
+        return_date = order.get_return_date()
+        while pickup_date <= return_date:
+            self.__reserved_dates.add(pickup_date)
+            pickup_date += timedelta(1)
+    
+    def add_to_history(self, order):
+        renter = order.get_customer_email()
+        pickup_date = order.get_pickup_date()
+        return_date = order.get_return_date()
+        self.__history[renter].append((pickup_date, return_date))
+        while pickup_date <= return_date:
+            self.__reserved_dates.remove(pickup_date)
+            pickup_date += timedelta(1)
+
+
+
+
+a = Car("BX-463", "jeppi", "rauður", "1998",\
+history = {"robertelis98@gmail.com": [(datetime(2018, 6, 10), datetime(2018, 6, 20))],\
+"liljag18@ru.is": [(datetime(2018, 6, 1), datetime(2018, 6, 9))]})
+

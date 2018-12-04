7095a940b92747e9d410dc312f730c08afc48dd2
Lobos <robertelis98@gmail.com>
2018-12-04 13:08:32 +0000

    Bj

--- models/Car.py
+++ models/Car.py
@@ -1,1 +1,23 @@
-print()
+
+
+class Car:
+    def __init__(self, reg_num, type, color, model, broken=False,\
+    history=None, reserved_dates=None):
+        self.__reg_num = reg_num
+        self.__type = type
+        self.__color = color
+        self.__model = model
+        self.__broken = broken
+        if history == None:
+            self.__history = dict()
+        else:
+            self.__history = history
+        if reserved_dates == None:
+            self.__reserved_dates = dict()
+        else:
+            self.__reserved_dates = reserved_dates
+            
+
+a = Car("BX-463", "Jeppi", "Rauður", "1998")
+
+print(a)

1933bdbe3be5a9fb85c8f9a3ecb1672ef1ad9f29
Matthildur <mattfrida@gmail.com>
2018-12-04 13:49:22 +0000

    set föll og number_of_days fall

--- models/Order.py
+++ models/Order.py
@@ -1,11 +1,14 @@
+from datetime import timedelta, datetime
+
 class Order:
     def __init__(self, order_no, car_reg_num, pickup_and_return_date,\
     customer_email, bonus_insurance):
         self.__order_no = order_no #gera fall til að gera pöntunarnr
         self.__car_reg_num = car_reg_num
-        self.__pickup_and_return_date = pickup_and_return_date #gera fall til að reikna fjölda daga
+        self.__pickup_and_return_date = pickup_and_return_date 
         self.__customer_email = customer_email
         self.__bonus_insurance = bonus_insurance
+
 
     def get_order_no(self):
         return self.__order_no
@@ -28,11 +31,16 @@ class Order:
     def get_date(self):
         return self.__pickup_and_return_date
 
-    def set_car_reg_num(self):
-        pass
+    def number_of_days(self):
+        nr_days = (self.get_return_date() - self.get_pickup_date()).days + 1
+        return nr_days
 
-    def set_customer_email(self):
-        pass
+    def set_car_reg_num(self, reg_num):
+        self.__car_reg_num = reg_num
 
-    def set_bonus_insurance(self):
-        pass
+    def set_customer_email(self, email):
+        self.__customer_email = email
+
+    def set_bonus_insurance(self, insurance_bool):
+        self.__bonus_insurance = insurance_bool
+        

4f9923fb996a115501008860ff11f159102b4d92
Lobos <robertelis98@gmail.com>
2018-12-05 14:39:33 +0000

    Lagaði CarRepo þannig að nú er Car input í föllin frekar en reg_num o.s.frv. og útfærði log_broken_car fallið að fullu

--- services/CarService.py
+++ services/CarService.py
@@ -13,8 +13,13 @@ class CarService:
                 found_car = Car(car[0], car[1], car[2], car[3], car[4], car[5], car[6])
                 return found_car
     
-    def log_broken_car(self):
-        ###Find car and mark as broken
+    def log_broken_car(self, reg_num):
+        ###Finnur bíl, fjarlægir hann úr gagnagrunni,\
+        ###breytir ástandinu og skrifar hann aftur í gagnagunninn
+        car_to_be_changed = self.find_car(reg_num)
+        self.__car_repo.delete_car(car_to_be_changed)
+        car_to_be_changed.change_broken_status()
+        self.__car_repo.add_car(car_to_be_changed)
         pass
 
     def log_new_car(self):
